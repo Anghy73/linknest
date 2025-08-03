0ba7c1baddbdf494d5484cc62564aaa45f7eae41
Andy <anghy.ruz@gmail.com>
2025-08-03 18:03:10 -0400

    shortId format for vercel

--- src/app/shortlink/[shortId]/page.tsx
+++ src/app/shortlink/[shortId]/page.tsx
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default async function ShortIdPage(props: {
   params: Promise<{
@@ -7,22 +8,36 @@ export default async function ShortIdPage(props: {
   }>;
 }) {
   const { shortId } = await props.params;
+  const [guestId, setGuestId] = useState<string>("");
+
+  useEffect(() => {
+    let storedGuestId = localStorage.getItem("guestId");
+    if (!storedGuestId) {
+      storedGuestId = crypto.randomUUID();
+      localStorage.setItem("guestId", storedGuestId);
+    }
+    setGuestId(storedGuestId);
+  }, []);
+
   const data = await prisma.shortLink.findUnique({
     where: {
-      shortUrl: shortId,
+      shortUrl_guestId: {
+        shortUrl: shortId,
+        guestId: guestId,
+      },
     },
   });
 
   console.log(data);
 
   if (!data) {
-    redirect('/')
+    redirect("/");
   }
 
-  redirect(data.url)
+  redirect(data.url);
   // return {
-    // redirect: {
-    //   destination: data.url,
-    // },
+  // redirect: {
+  //   destination: data.url,
+  // },
   // };
 }
