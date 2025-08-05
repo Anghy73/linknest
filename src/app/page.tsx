2112fd395c08f9d1cd304b2313b1f91f6aa48243
Andy <anghy.ruz@gmail.com>
2025-08-05 10:08:45 -0400

    link user dinamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
+  const user = 'user'
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>This will be the landing pageaa</h1>
-        <Link className={buttonVariants({ variant: "destructive" })} href="/user">Go to App</Link>
+        <Link className={buttonVariants({ variant: "destructive" })} href={`/${user}`}>Go to App</Link>
       </main>
       {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer> */}
